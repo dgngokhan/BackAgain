679877a3bbfe52d54b76e1822f3587f15a6df5d1
dgngokhan <dogangokhan@yahoo.com>
2023-04-05 10:36:49 -0400

    third commit

---
+++ src/test/java/Second_Day.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Second_Day {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your salary: ");
+        double salary = scan.nextDouble();
+        scan.nextLine();
+
+
+        System.out.println("Enter your full name: ");
+
+        String fullName =  scan.nextLine();
+
+        System.out.println("Salary: "+salary);
+        System.out.println("full name: "+fullName);
+    }
+
+
+}
